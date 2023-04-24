6245574a794e1dc24218c6fcc6055896bb892228
Aceroph <j.pilotte92@gmail.com>
2023-04-23 22:00:10 -0400

    bot class and help command class

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+from discord.ext import commands
 import discord
 import os
 from dotenv import load_dotenv
 
 load_dotenv('.env')
-intents = discord.Intents.default()
-intents.message_content = True
 
-client = discord.Client(intents=intents)
+def prefix(bot: commands.Bot, msg):
+    client_id = bot.user.id
+    return ['a.', f'<@!{client_id}>', f'<@{client_id}>']
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+class AceHelp(commands.HelpCommand):
 
-    if message.content.startswith('$ping'):
-        await message.channel.send('pong!')
+    async def send_bot_help(self, mapping):
+        filtered = await self.filter_commands(self.context.bot.commands, sort=True)
+        names = [command.name for command in filtered]
+        available_commands = "\n".join(names)
+        embed = discord.Embed(description=available_commands)
+        await self.context.send(embed=embed)
 
-client.run(os.environ.get('TOKEN'))
+    async def send_error_message(self, error):
+        await self.get_destination().send(error)
+
+
+class AceBot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(command_prefix=prefix, intents=discord.Intents.all(), help_command=AceHelp())
+
+    async def on_ready(self):
+        print(f'Connected as {self.user} (ID: {self.user.id})')
+
+
+bot = AceBot()
+bot.run(os.environ.get('TOKEN'))
